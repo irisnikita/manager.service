6788e57961aff0b39475b63461ed8e5458444f8e
Truong vi <vinlt@antsprogrammatic.com>
2020-07-03 14:02:43 +0700

    Update port

--- redis.js
+++ redis.js
@@ -2,7 +2,10 @@ const redis = require('redis');
 
 const PORT_REDIS = process.env.PORT || 6379;
 
-const redis_client = redis.createClient(PORT_REDIS)
+const redis_client = redis.createClient({
+    port: PORT_REDIS,
+    host: '104.248.231.127'
+})
 redis_client.on('error', (error) => {
     console.log(error)
 })
